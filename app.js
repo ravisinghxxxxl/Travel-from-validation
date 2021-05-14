c72917dbcfec2cc410999a3b4415e5f3e52e6367
Ravi <ravisinghxxxxl@gmail.com>
2021-05-14 11:33:40 +0300

    name, email and phone NO. validation added

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
-console.log('hello');
-const names = document.getElementById('userName')
-const email = document.getElementById('email')
-const phone = document.getElementById('phone')
+console.log("hello");
+const names = document.getElementById("userName");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
 
+names.addEventListener("blur", () => {
+  // NAME VALIDATION
+  let regex = /^[a-zA-Z]([0-9a-zA-Z]){2,10}$/;
+  let nameVal = names.value;
+  if (regex.test(nameVal)) {
+    names.classList.remove("is-invalid");
+  } else {
+    names.classList.add("is-invalid");
+  }
+});
 
-names.addEventListener('blur', ()=> {
-    console.log("name is blurred!");
+email.addEventListener("blur", () => {
+  // EMAIL VALIDATION
+  let emailVal = email.value;
+  let regex = /^([_\-\.a-zA-Z0-9]+)@([_\-\.a-zA-Z0-9]+)\.([a-zA-Z]){2,7}$/;
+  if (regex.test(emailVal)) {
+    email.classList.remove("is-invalid");
+  } else {
+    email.classList.add("is-invalid");
+  }
+});
 
-    // NAME VALIDATION
-    let regex = /^[a-zA-Z]([0-9A-Za-z]){0,10}$ /
-    let str = names.value;
-if(regex.test(str)) {
-    console.log('it matched');
-    names.classList.remove('is-invalid')
-} else {
-    console.log('Wrong input');
-    names.classList.add('is-invalid')
-}
-})
-
-email.addEventListener('blur', ()=> {
-    console.log("email is blurred!");
-    // EMAIL VALIDATION
-})
-phone.addEventListener('blur', ()=> {
-    console.log("phone is blurred!");
-    // PHONE NO. VALIDATION
-})
- 
+phone.addEventListener("blur", () => {
+  // PHONE NO. VALIDATION
+  let phoneVal = phone.value;
+  let regex = /^([0-9]){2,10}$/;
+  if (regex.test(phoneVal)) {
+    phone.classList.remove("is-invalid");
+  } else {
+    phone.classList.add("is-invalid");
+  }
+});
